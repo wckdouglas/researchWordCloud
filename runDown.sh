13ccaba0192f437c372adc31598eab1261e5aee8
Douglas Wu <wckdouglas@gmail.com>
2015-10-05 12:41:01 -0500

    added file

--- runDown.sh
+++ runDown.sh
@@ -1,8 +1,14 @@
-#/bin/bash
+#!/bin/bash
 
 export LC_ALL="C"
+
+#pdftoText:
 #parallel pdftotext {} ::: *pdf
+
+#getWordCount
 cat *txt | python wordCount.py | cut -d',' -f1 | sort | uniq -c | awk '{print $2,$1}' | sort -k2n |  tr ' ' ',' > wordCount.csv
+
+#plotting:
 Rscript wordCloud.R
 
 
--- wordCloud.R
+++ wordCloud.R
@@ -12,9 +12,10 @@ df <- read_csv('wordCount.csv',col_names=c('word','count')) %>%
 	mutate(word = tolower(word)) %>%
 	group_by(word) %>%
 	summarize(count = sum(count)) %>%
-	filter(!grepl('and|or|to|the|an|a|in|of|that|were|is|on|by|with|not|be|from',word))
+	filter(!grepl('and|or|to|the|an|a|in|of|that|were|is|on|by|with|not|be|from',word)) %>%
+	filter(word!='cell')
 
 pdf("wordcloud.pdf", 10,10)
 wordcloud(df$word,df$count,color=colorsScheme,max.words=800,
-		  scale=c(8,.2), random.order=FALSE, rot.per=.15)
+		  scale=c(8,.2), random.order=FALSE, rot.per=.15,random.color=T)
 dev.off()
