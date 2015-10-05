42c4380cf28d0e22c1a8f0440d351eb14e44a62e
Douglas Wu <wckdouglas@gmail.com>
2015-10-05 00:47:14 -0500

    changed wordcloud options

--- wordCloud.R
+++ wordCloud.R
@@ -5,16 +5,18 @@ library(dplyr)
 library(wordcloud)
 library(RColorBrewer)
 
-colorsScheme <- brewer.pal(8,"Paired")
+colorsScheme <- brewer.pal(8,"Set2")
 df <- read_csv('wordCount.csv',col_names=c('word','count')) %>%
 	filter(!grepl('\\.|\\||\\(|\\#|\\&|\\*|\\+|\\/|\\)|\\!|\\-|\\=| |\\"|\\`',word)) %>%
 	filter(!grepl('[0-9]',word)) %>%
 	mutate(word = tolower(word)) %>%
 	group_by(word) %>%
 	summarize(count = sum(count)) %>%
-	filter(!grepl('and|or|to|the|an|a|in|of|that|were|is|on|by|with|not|be|from',word))
+	filter(!grepl('and|or|to|the|an|a|in|of|that|were|is|on|by|with|not|be|from',word))  %>%
+	filter(word!='cell')
 
 pdf("wordcloud.pdf", 10,10)
-wordcloud(df$word,df$count,color=colorsScheme,max.words=800,
-		  scale=c(8,.2), random.order=FALSE, rot.per=.15)
+wordcloud(df$word,df$count,colors=colorsScheme,max.words=800,
+		  scale=c(8,.2), random.order=FALSE, rot.per=.15,
+		  random.color=T)
 dev.off()
